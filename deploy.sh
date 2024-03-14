c2c8db24053336a42ec28e75b1769a3f32b05d7f
Victor Madera <v.maderaalbors@gmail.com>
2024-03-14 21:18:27 +0000

    changing deploy.sh to main folder

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+# Install pm2 which is a production process manager for Node.js with a built-in load balancer.
+sudo npm install -g pm2
+# stop any instances of our application running currently
+pm2 stop SimpleApplication
+# change directory into folder where application is dowloaded
+cd SimpleApplication/
+# Install application dependencies 
+npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+# Start the application with the process name SimpleApplication using pm2
+pm2 start ./bin/www --name SimpleApplication
